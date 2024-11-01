b95b089169bdabd1f39d4c6c56d103ac3bc0f2fb
CodingWizard2001 <goswami.rahul1002@gmail.com>
2024-11-01 18:37:52 +0530

    Delete File

--- FlightBooking/Assests/JavaScript/script.js
+++
@@ -1,9 +0,0 @@
-// Swiper JS Code
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-});
